13b1f719ba9c82e99ff3d6ddacd91d36ca8495e6
TornOne <ott.adermann@gmail.com>
2017-04-22 17:04:20 +0300

    sync

---
+++ Assets/Scripts/ProjectileAttacker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileAttacker : MonoBehaviour {
+	public float timeout;
+	public float speed;
+	Vector2
+
+	void Start () {
+		 = transform.eulerAngles.z / 180 * Mathf.PI;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
