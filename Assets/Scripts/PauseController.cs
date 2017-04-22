ad9092487666e9ff508b1fcbd80521c9438b2d0c
Marko Korbun <madauh@gmail.com>
2017-04-22 21:07:31 +0300

    PauseController + knight sprites

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour {
+
+    public bool isGamePaused = false;
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+
+    public void PauseGame()
+    {
+        if(!isGamePaused)
+        {
+            Time.timeScale = 0;
+            isGamePaused = true;
+        }
+        else if(isGamePaused)
+        {
+            Time.timeScale = 1;
+            isGamePaused = false;
+        }
+
+    }
+}
