bf74fae3436aa1c8b132ee179ceacffe3568866e
Joosep Jääger <joosep.jaager@gmail.com>
2017-04-22 14:29:53 +0300

    Added attack reticule. Added background.

--- Assets/Attack.cs
+++ Assets/Attack.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Attack : MonoBehaviour {
 
+	private Animator anim;
 
+	public GameObject reticule;
+
+	void Start(){
+		anim = GetComponent<Animator> ();
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown ("LightAttack")) {
+			anim.SetTrigger ("LightAttack");
+		}
+		if (Input.GetButtonDown ("HeavyAttack")) {
+			anim.SetTrigger ("HeavyAttack");
+		}
+		reticule.transform.rotation = Quaternion.FromToRotation (Vector3.down, Input.mousePosition - new Vector3(Screen.width/2, Screen.height/2));
 	}
 }
