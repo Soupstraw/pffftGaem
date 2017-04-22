a58c6e56dddd874acb07951d21004871a4c854c7
TornOne <ott.adermann@gmail.com>
2017-04-22 17:27:45 +0300

    SampleAI

--- Assets/Scripts/ProjectileAttacker.cs
+++ Assets/Scripts/ProjectileAttacker.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Attacker))]
 public class ProjectileAttacker : MonoBehaviour {
 	public float timeout;
 	public float speed;
-	Vector2
+	Vector3 direction;
+
+	Attacker attacker;
 
 	void Start () {
-		 = transform.eulerAngles.z / 180 * Mathf.PI;
+		Destroy (gameObject, timeout);
+		float rotation = transform.eulerAngles.z / 180 * Mathf.PI;
+		direction = new Vector3(Mathf.Cos(rotation), Mathf.Sin(rotation), 0) * speed;
+		attacker = gameObject.GetComponent<Attacker>();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+		transform.Translate (direction);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		attacker.DealDamage ();
 	}
 }
