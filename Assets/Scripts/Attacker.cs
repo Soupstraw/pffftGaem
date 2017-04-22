b14ac6babe5f12e81e33bb57fd47170cae31009b
Joosep Jääger <joosep.jaager@gmail.com>
2017-04-22 15:58:16 +0300

    Added damage scripts.

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Attacker : MonoBehaviour {
+
+	public float damage;
+
+	private Collider2D collider;
+
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<Collider2D>();
+	}
+	
+	public void DealDamage(){
+		ContactFilter2D filter = new ContactFilter2D ();
+		Collider2D[] res = new Collider2D[100];
+		Physics2D.OverlapCollider (collider, filter, res);
+		foreach(Collider2D col in res){
+			Damagable dam = col.GetComponent<Damagable> ();
+			if (dam != null) {
+				dam.DealDamage (damage);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Damagable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damagable : MonoBehaviour {
+
+	public float health = 100f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DealDamage(float damage){
+		health -= damage;
+		Debug.Log ("Health: " + health);
+		if (health <= 0) {
+			Kill ();
+		}
+	}
+
+	public void Kill(){
+		Destroy (gameObject);
+	}
+}
