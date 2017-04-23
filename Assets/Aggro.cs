844af22722ae06a906f7e3aeddada3d06608d9f8
Joosep Jääger <joosep.jaager@gmail.com>
2017-04-23 13:09:47 +0300

    Added aggro trigger.

---
+++ Assets/Aggro.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Aggro : MonoBehaviour {
+
+	private Collider2D collider;
+
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<Collider2D> ();
+		SetAIEnabled (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D c){
+		if(c.gameObject.tag == "Player"){
+			SetAIEnabled (true);
+		}
+	}
+
+	void SetAIEnabled(bool enable){
+		ContactFilter2D filter = new ContactFilter2D ();
+		LayerMask mask = LayerMask.GetMask ("Enemy");
+		filter.SetLayerMask (mask);
+		Collider2D[] res = new Collider2D[100];
+		Physics2D.OverlapCollider (collider, filter, res);
+		foreach(Collider2D col in res){
+			if (col != null) {
+				AI ai = col.GetComponent<AI> ();
+				if (ai != null) {
+					ai.enabled = enable;
+				}
+			}
+		}
+	}
+}
