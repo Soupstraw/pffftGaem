39336ffe7f2b68aaa82ba35d3f91a94e608f9131
Joosep Jääger <joosep.jaager@gmail.com>
2017-04-24 15:03:44 +0300

    ZSorter.

---
+++ Assets/Scripts/ZSorter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZSorter : MonoBehaviour {
+
+	public float ratio = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.y * ratio);
+	}
+}
