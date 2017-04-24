28332bdcc36def7d489de70d307bde9b8f0b4c6d
Joosep Jääger <joosep.jaager@gmail.com>
2017-04-24 14:12:53 +0300

    Swipe particles and balancing.

---
+++ Assets/ParticleKill.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleKill : MonoBehaviour {
+
+	private ParticleSystem part;
+
+	// Use this for initialization
+	void Start () {
+		part = GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (part.isStopped) {
+			Destroy (gameObject);
+		}
+	}
+}
